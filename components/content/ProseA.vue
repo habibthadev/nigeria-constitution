5d10e8a1113b877a51969fffba2adca63c680d12
habibthadev <adebayohabib7@gmail.com>
2026-03-11 14:59:05 +0100

    feat: add ProseA markdown renderer component

---
+++ components/content/ProseA.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  href?: string
+  target?: string
+  rel?: string
+}>()
+</script>
+<template>
+  <a :href="href" :target="target" :rel="rel"><slot /></a>
+</template>
