4564d6bae03c996deaaa987b4ba9d696f1122b1b
habibthadev <adebayohabib7@gmail.com>
2026-03-11 14:59:06 +0100

    feat: add ProsePre markdown renderer component

---
+++ components/content/ProsePre.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  code?: string
+  language?: string
+  filename?: string
+  highlights?: number[]
+  meta?: string
+}>()
+</script>
+<template>
+  <pre><slot /></pre>
+</template>
