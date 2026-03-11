4334f44466b1e1a0cf5c388b0ba222070a817aec
habibthadev <adebayohabib7@gmail.com>
2026-03-11 14:58:26 +0100

    feat: add root app layout with page transitions

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div style="background: var(--bg); color: var(--text-primary); min-height: 100vh; display: flex; flex-direction: column;">
+    <AppHeader />
+    <main style="flex: 1;">
+      <NuxtPage />
+    </main>
+    <AppFooter />
+  </div>
+</template>
